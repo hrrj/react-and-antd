6907b16bf11286d78e183fa97b7e29766793f31d
hrrj <810390198@qq.com>
2018-03-29 18:04:12 +0800

    初始化项目结构

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -157,13 +157,14 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(css|less)$/,
             use: [
               require.resolve('style-loader'),
               {
                 loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1,
+                  importLoaders: 1,                  
+                  sourcemap: true,
                 },
               },
               {
@@ -184,8 +185,15 @@ module.exports = {
                       flexbox: 'no-2009',
                     }),
                   ],
+                  sourceMap: true,
                 },
               },
+              {
+                loader: require.resolve('less-loader'),
+                options: {
+                  sourceMap: true,
+                }
+              }
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
 import { LocaleProvider} from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
+import registerServiceWorker from './registerServiceWorker';
+
+import Home from './pages/Home/Home';
+import BasicLayout from './layouts/BasicLayout'
+import './index.less';
+
 moment.locale('zh-cn');
 
-ReactDOM.render(<LocaleProvider locale={zhCN}><App /></LocaleProvider>, document.getElementById('root'));
+class App extends React.Component{
+    render(){
+        return(
+            <LocaleProvider locale={zhCN}>
+                    <BasicLayout>
+                        <Home />
+                    </BasicLayout>
+            </LocaleProvider>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
