1d005115c84100e4ee07a4c7fff8562f8449e0fd
Hlin <810390198@qq.com>
2018-04-18 23:03:01 +0800

    完成登录页布局

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { changeAuthority } from '../../store/user/action'
-// import style from './Login.less'
+import { Form, Icon, Input, Button, Row, Col } from 'antd';
+import style from './Login.less'
+const FormItem = Form.Item;
 
 class Login extends React.Component{
     loginTo(){
@@ -12,11 +14,25 @@ class Login extends React.Component{
     }
     render(){
         return(
-            <div className="login">
-                <h2>登录页面</h2>
-                <p>当前登录状态：{this.props.userData.athority}</p>
-                <button onClick={(e) => {this.loginTo()}}>登录</button>
-            </div>
+            <Row className={style.login} type="flex" align="middle">
+                <Col xs={24} sm={{span:12, offset:6}} md={{span:8, offset:8}} lg={{span:8, offset:8}} xl={{span:6, offset:9}}>
+                    <Form className={style.form} onSubmit={this.handleSubmit}>
+                        <FormItem className={style.logoBox}>
+                            <Icon className={style.logo} type='api' />
+                        </FormItem>
+                        <FormItem>
+                            <Input prefix={<Icon type="user"/>} placeholder="Username" />
+                        </FormItem>
+                        <FormItem>
+                            <Input prefix={<Icon type="lock"/>} type="password" placeholder="Password" />
+                        </FormItem>
+                        <Button className={style.submitBtn} type="primary" htmlType="submit">
+                            Log in
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+            
         )
     }
 }
